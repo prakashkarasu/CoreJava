61a2884453222c455bae8d48ae92d3ff8421b7e4
Prakash Thirunavukarasu <prakash.karasu@icloud.com>
2021-10-09 16:39:07 +0530

    Testing the Solution to the Temprature Problem

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,23 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        if(args.length != 2) exit();
+        double value = Double.parseDouble(args[0]);
+        char scale = Character.toUpperCase(args[1].charAt(0));
+        if (scale != 'C' && scale != 'F') exit();
+        Temperature temperature = new MyTemperature(value, scale);
+        System.out.println(temperature);
+    }
+
+    private static void exit()
+    {
+        System.out.println(
+                "Usage: java Covert <temperature> <scale>"
+                + "\nwhere:"
+                +"\t<temperature> is the temperature that you want to convert"
+                +"\n\t<scale> is either \"C\" or \"F\"."
+                +"\nExample: java Convert 67 F"
+        );
+        System.exit(0);
     }
 }
